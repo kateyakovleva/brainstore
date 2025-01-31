1fc1067e3903fb9564f493458e9ae0bfebfdd31b
Kate Yakovleva <105729527+kateyakovleva@users.noreply.github.com>
2025-01-31 10:29:41 +0300

    dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,17 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  // ngAfterViewInit(): void {
+  //   document.documentElement.setAttribute('data-theme', 'dark');
+  // }
+
+  ngAfterViewInit(): void {
+    const currentHour = new Date().getHours();
+
+    if (currentHour <= 6 && currentHour > 18) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+  }
 
 }
