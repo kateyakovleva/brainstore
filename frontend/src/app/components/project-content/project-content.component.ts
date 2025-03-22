cf5d7a456648458b30c9e8643b033b7becccf31b
Kate Yakovleva <105729527+kateyakovleva@users.noreply.github.com>
2025-03-22 21:22:17 +0300

    update all

--- frontend/src/app/components/project-content/project-content.component.ts
+++ frontend/src/app/components/project-content/project-content.component.ts
@@ -1,17 +1,17 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { MarkdownComponent } from 'ngx-markdown';
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import { StatisticsComponent } from '../../pages/components/statistics/statistics.component';
-import { WorksComponent } from '../../pages/components/works/works.component';
-import { FormComponent } from '../../pages/components/form/form.component';
-import { isMobile, videoUrl } from '../../utils/utils';
-import { IProject } from '../../types/types';
-import { ActivatedRoute } from '@angular/router';
-import { ProjectsStore } from '../../services/ProjectsStore';
-import { Observable } from 'rxjs';
-import { SafePipe } from '../../pipes/SafePipe';
+import {AfterViewInit, Component} from '@angular/core';
+import {MarkdownComponent} from 'ngx-markdown';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {StatisticsComponent} from '../../pages/components/statistics/statistics.component';
+import {WorksComponent} from '../../pages/components/works/works.component';
+import {FormComponent} from '../../pages/components/form/form.component';
+import {isMobile, videoUrl} from '../../utils/utils';
+import {IProject} from '../../types/types';
+import {ActivatedRoute} from '@angular/router';
+import {ProjectsStore} from '../../services/ProjectsStore';
+import {Observable} from 'rxjs';
+import {SafePipe} from '../../pipes/SafePipe';
 
-@Component( {
+@Component({
   selector: 'app-project-content',
   standalone: true,
   imports: [
@@ -26,7 +26,7 @@ import { SafePipe } from '../../pipes/SafePipe';
   ],
   templateUrl: './project-content.component.html',
   styleUrl: './project-content.component.scss'
-} )
+})
 export class ProjectContentComponent implements AfterViewInit {
 
   isMobile = isMobile;
@@ -35,9 +35,9 @@ export class ProjectContentComponent implements AfterViewInit {
     private location: ActivatedRoute,
     private projectsStore: ProjectsStore,
   ) {
-    location.params.subscribe( ( params ) => {
-      this.project = this.projectsStore.getProject( params[ 'id' ] );
-    } );
+    location.params.subscribe((params) => {
+      this.project = this.projectsStore.getProject(params['id']);
+    });
   }
 
   project = new Observable<IProject>();
@@ -48,16 +48,17 @@ export class ProjectContentComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    if ( typeof window !== undefined ) {
+    if (typeof window !== undefined) {
+      const size = window.innerWidth;
       this.size = {
-        width: window.innerWidth * 0.9,
-        height: window.innerHeight * 0.9,
+        width: size,
+        height: size / 16 * 9,
       }
     }
   }
 
-  url( url: string ) {
-    return videoUrl( url );
+  url(url: string) {
+    return videoUrl(url);
   }
 }
 
