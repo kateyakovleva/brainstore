a7262b15c6f62bdbd061752f312431c3778217a2
Kate Yakovleva <105729527+kateyakovleva@users.noreply.github.com>
2025-03-24 22:44:08 +0300

    update

--- frontend/src/app/components/slider/slider.component.ts
+++ frontend/src/app/components/slider/slider.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { IProject } from '../../types/types';
-import { RouterLink } from '@angular/router';
-import { NgClass, NgForOf } from '@angular/common';
-import { ProjectsStore } from '../../services/ProjectsStore';
-import { MarkdownComponent } from 'ngx-markdown';
+import {Component, OnInit} from '@angular/core';
+import {IProject} from '../../types/types';
+import {RouterLink} from '@angular/router';
+import {NgClass, NgForOf} from '@angular/common';
+import {ProjectsStore} from '../../services/ProjectsStore';
+import {MarkdownComponent} from 'ngx-markdown';
 
-@Component( {
+@Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [ RouterLink, NgForOf, NgClass, MarkdownComponent ],
+  imports: [RouterLink, NgForOf, NgClass, MarkdownComponent],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
-} )
+})
 export class SliderComponent implements OnInit {
   constructor(
     public projectsStore: ProjectsStore
   ) {
-    projectsStore.projects.subscribe( ( s ) => {
+    projectsStore.projects.subscribe((s) => {
       this.slides = s;
-    } )
+    })
   }
 
   ngOnInit() {
-    if ( !this.inited ) {
-      setTimeout( () => {
+    if (!this.inited) {
+      setTimeout(() => {
         const width = typeof window !== "undefined" ? window.innerWidth : 1300;
-        const contWidth = ( typeof document !== "undefined" ? document.querySelector( '.container' )?.clientWidth : 1300 ) || 1300;
-        this.width = `${ ( width - contWidth ) / 2 + contWidth }px`;
-      }, 100 );
+        const contWidth = (typeof document !== "undefined" ? document.querySelector('.container')?.clientWidth : 1300) || 1300;
+        this.width = `${(width - contWidth) / 2 + contWidth}px`;
+      }, 100);
       this.inited = true;
     }
   }
@@ -44,10 +44,10 @@ export class SliderComponent implements OnInit {
   // @ts-ignore
   scrollLeft: number;
 
-  onMouseDown( event: MouseEvent ) {
+  onMouseDown(event: MouseEvent) {
     this.isMouseDown = true;
-    this.startX = event.pageX - ( event.currentTarget as HTMLElement ).offsetLeft;
-    this.scrollLeft = ( event.currentTarget as HTMLElement ).scrollLeft;
+    this.startX = event.pageX - (event.currentTarget as HTMLElement).offsetLeft;
+    this.scrollLeft = (event.currentTarget as HTMLElement).scrollLeft;
   }
 
   onMouseLeave() {
@@ -58,12 +58,12 @@ export class SliderComponent implements OnInit {
     this.isMouseDown = false;
   }
 
-  onMouseMove( event: MouseEvent ) {
-    if ( !this.isMouseDown ) return;
+  onMouseMove(event: MouseEvent) {
+    if (!this.isMouseDown) return;
     event.preventDefault();
-    const x = event.pageX - ( event.currentTarget as HTMLElement ).offsetLeft;
-    const walk = ( x - this.startX ) * 2; // Скорость прокрутки
-    ( event.currentTarget as HTMLElement ).scrollLeft = this.scrollLeft - walk;
+    const x = event.pageX - (event.currentTarget as HTMLElement).offsetLeft;
+    const walk = (x - this.startX) * 2; // Скорость прокрутки
+    (event.currentTarget as HTMLElement).scrollLeft = this.scrollLeft - walk;
   }
 
 
